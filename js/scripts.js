80b8991b1e119070b95f03ab9402c2836180f484
Snulu <Nnabwami@gmail.com>
2019-01-15 21:43:02 -0800

    Add id to row elements.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,31 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $("#initially-hidden").toggle();
-    $("#initially-showing").toggle();
-    $("#definition").fadeToggle("slow");
+  $("#js .clickable").click(function() {
+    $("#js #initially-hidden").toggle();
+    $("#js #initially-showing").toggle();
+    $("#js #definition").fadeToggle("slow");
+  });
 
+  $("#op .clickable").click(function() {
+    $("#op #initially-hidden").toggle();
+    $("#op #initially-showing").toggle();
+    $("#op #definition").fadeToggle("slow");
+  });
+
+  $("#va .clickable").click(function() {
+    $("#va #initially-hidden").toggle();
+    $("#va #initially-showing").toggle();
+    $("#va #definition").fadeToggle("slow");
+  });
+
+  $("#co .clickable").click(function() {
+    $("#co #initially-hidden").toggle();
+    $("#co #initially-showing").toggle();
+    $("#co #definition").fadeToggle("slow");
+  });
+
+  $("#as .clickable").click(function() {
+    $("#as #initially-hidden").toggle();
+    $("#as #initially-showing").toggle();
+    $("#as #definition").fadeToggle("slow");
   });
 });
